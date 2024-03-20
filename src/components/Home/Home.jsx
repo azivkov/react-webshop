33bacf305044dbaebc980750966d3c334bfbe170
azivkov <anamaria.zivkovic@gmail.com>
2024-03-20 19:16:03 +0100

    updated links

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,20 @@ import { HomeBottom } from './HomeBottom';
 import { productData } from '../Products/productData';
 
 function Home({handleAddToCart}) {
-    const CategoriesOne = ["Mobiteli", "Stolna računala", "Laptopi", "Televizori", "Printeri"];
-    const CategoriesTwo = ["Konzole", "Igre", "Tipkovnice", "Pohrana podataka", "Mrežna oprema"];
+    const CategoriesOne = [
+        {title: "Mobiteli", link: "mobiteli"},
+        {title: "Stolna računala", link:"stolnaracunala"},
+        {title: "Laptopi", link: "laptopi"},
+        {title: "Televizori", link: "televizori"},
+        {title: "Printeri", link: "printeri"}
+    ];
+    const CategoriesTwo = [
+        {title: "Konzole", link: "konzole"},
+        {title: "Igre", link: "videoigre"},
+        {title: "Tipkovnice", link: "tipkovnice"},
+        {title: "Pohrana podataka", link: "pohrana"},
+        {title: "Mrežna oprema", link: "mreznaoprema"}
+    ];
     const PopularProducts = [...productData].slice(0,5);
 
     return(
--- src/components/Home/HomeCategories.jsx
+++ src/components/Home/HomeCategories.jsx
@@ -1,16 +1,18 @@
+import { Link } from "react-router-dom"
+
 function HomeCategories({title, categoriesOne, categoriesTwo}) {
     return(
         <div className={"proizvodi"}>
         <h2 className={"secondary-font main-text-color"}>{title}</h2>
         <ul className={"katproizvodi"}>
             {categoriesOne.map((item, index) => (
-                <li key={index}><button>{item}</button></li>
+                <li key={index}><Link to={"/products?kategorija="+item.link}><button>{item.title}</button></Link></li>
             ))}
         </ul>
         <ul className={"katproizvodi2"}>
             {categoriesTwo.map((item, index) => (
-                <li key={index}><button>{item}</button></li>
-            ))}
+                <li key={index}><Link to={"/products?kategorija="+item.link}><button>{item.title}</button></Link></li>
+                ))}
         </ul>
     </div>
     )
