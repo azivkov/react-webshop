09a6c22b45045cfd3eb176e8d2905be594bb760e
azivkov <anamaria.zivkovic@gmail.com>
2024-03-20 18:18:36 +0100

    adjustments

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -4,11 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
 
 function ProductList({productData, handleAddToCart, matchedProducts, objectParams}) {
+    const listOfProducts = Object.keys(objectParams).length !== 0 ? matchedProducts : productData;
+
     return(
         <div className="products">
-            {Object.keys(objectParams).length !== 0 ? (
                 <ul>
-                    {matchedProducts.map((item, index) => (
+                    {listOfProducts.map((item, index) => (
                         <li key={index}>
                             <Link to={"/products/"+item.id}>
                                 <img src={item.image} alt={item.title} title={item.title} />
@@ -19,20 +20,6 @@ function ProductList({productData, handleAddToCart, matchedProducts, objectParam
                         </li>
                     ))}
                 </ul>
-            ) : (
-                <ul>
-                    {productData.map((item, index) => (
-                        <li key={index}>
-                            <Link to={"/products/"+item.id}>
-                                <img src={item.image} alt={item.title} title={item.title} />
-                                <h3 className="secondary-font">{item.title}</h3>
-                            </Link>
-                            <p className="main-font">{item.price} €</p>
-                            <button onClick={() => handleAddToCart(item)}>Dodaj u košaricu</button>
-                        </li>
-                    ))}
-                </ul>
-            )}
             <section className="nextpage">
                 <div data-status="disabled"><FontAwesomeIcon icon={faBackward} className="fa-solid fa-backward" /></div>
                 <div>1</div>
