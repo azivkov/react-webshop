b9bd03f91ad3aeb25cfc553f3f173024d77f3cc3
azivkov <anamaria.zivkovic@gmail.com>
2024-04-15 21:05:56 +0200

    changed some category names

--- src/components/Products/ProductsPage.jsx
+++ src/components/Products/ProductsPage.jsx
@@ -1,62 +1,64 @@
-import React from 'react';
-import './ProductsPage.css';
-import { images } from '../../assets/images';
+import React from "react";
+import "./ProductsPage.css";
+import { images } from "../../assets/images";
 import { Link, useSearchParams } from "react-router-dom";
-import { ProductFilters } from './ProductFilters';
-import { ProductList } from './ProductList';
-import { productData } from './productData';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faForward, faBackward } from '@fortawesome/free-solid-svg-icons';
-
+import { ProductFilters } from "./ProductFilters";
+import { ProductList } from "./ProductList";
+import { productData } from "./productData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faForward, faBackward } from "@fortawesome/free-solid-svg-icons";
 
 function ProductsPage() {
-    const priceFilter = [
-        {value: "do49", title: "Do 49€"},
-        {value: "50do199", title: "50€ - 199€"},
-        {value: "200do499", title: "200€ - 499€"},
-        {value: "iznad500", title: "Iznad 500€"},
-    ]
+  const priceFilter = [
+    { value: "do49", title: "Do 49€" },
+    { value: "50do199", title: "50€ - 199€" },
+    { value: "200do499", title: "200€ - 499€" },
+    { value: "iznad500", title: "Iznad 500€" },
+  ];
 
-    const categoryFilter = [
-        {value: "mobiteli", title: "Mobiteli"},
-        {value: "stolnaracunala", title: "Stolna računala"},
-        {value: "laptopi", title: "Laptopi"},
-        {value: "televizori", title: "Televizori"},
-        {value: "konzole", title: "Konzole"},
-        {value: "videoigre", title: "Videoigre"},
-        {value: "printeri", title: "Printeri"},
-        {value: "tipkovnice", title: "Tipkovnice"},
-        {value: "pohrana", title: "Pohrana podataka"},
-        {value: "mreznaoprema", title: "Mrežna oprema"},
-    ]
+  const categoryFilter = [
+    { value: "mobiteli", title: "Mobiteli" },
+    { value: "stolnaracunala", title: "Stolna računala" },
+    { value: "laptopi", title: "Laptopi" },
+    { value: "televizori", title: "Televizori" },
+    { value: "konzole", title: "Konzole" },
+    { value: "videoigre", title: "Videoigre" },
+    { value: "printeri", title: "Printeri" },
+    { value: "tipkovnice", title: "Tipkovnice" },
+    { value: "pohrana", title: "Pohrana podataka" },
+    { value: "mreznaoprema", title: "Mrežna oprema" },
+  ];
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const objectParams = Object.fromEntries([...searchParams]);
-    // console.log("objectParams", objectParams);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const objectParams = Object.fromEntries([...searchParams]);
+  // console.log("objectParams", objectParams);
 
-    const matchedProducts = productData.filter(product => {
-    // console.log("product cijena", product.cijena);
-    // console.log("objectParams cijena", objectParams.cijena);
+  const matchedProducts = productData.filter((product) => {
+    // console.log("product range", product.range);
+    // console.log("objectParams range", objectParams.range);
     return (
-        (product.cijena === objectParams.cijena || !objectParams.cijena) && 
-        (product.kategorija === objectParams.kategorija || !objectParams.kategorija)
+      (product.range === objectParams.cijena || !objectParams.cijena) &&
+      (product.category === objectParams.kategorija || !objectParams.kategorija)
     );
-    })
+  });
 
-    return (
-        <main>
-            <h1 className="main-text-color">Proizvodi</h1>
-            <div className="productlist-container">
-                <ProductFilters priceFilter={priceFilter} categoryFilter={categoryFilter} />
+  return (
+    <main>
+      <h1 className="main-text-color">Proizvodi</h1>
+      <div className="productlist-container">
+        <ProductFilters
+          priceFilter={priceFilter}
+          categoryFilter={categoryFilter}
+        />
 
-                <ProductList productData={productData} 
-                objectParams={objectParams}
-                matchedProducts={matchedProducts}/>
-            </div>
-        </main>
-
-        
-    );
+        <ProductList
+          productData={productData}
+          objectParams={objectParams}
+          matchedProducts={matchedProducts}
+        />
+      </div>
+    </main>
+  );
 }
 
 export { ProductsPage };
