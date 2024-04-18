db574dc762289c9deaa91c2e11fe0690f8b0a782
azivkov <anamaria.zivkovic@gmail.com>
2024-04-18 22:08:55 +0200

    updated checkout form

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -4,12 +4,21 @@ import { CheckoutAddress } from './CheckoutAddress';
 import { CheckoutCart } from './CheckoutCart';
 
 function Checkout({title}) {
+    const checkoutForm = [
+        {title: "Ime i prezime:", type: "text", id: "ime"},
+        {title: "E-mail adresa", type: "mail", id:"email"},
+        {title: "Kućna adresa:", type: "text", id: "adresa"},
+        {title: "Grad:", type: "text", id: "grad"},
+        {title: "Poštanski broj:", type: "number", id: "postbroj"},
+        {title: "Država:", type: "text", id: "drzava"},
+        {title: "Telefon:", type: "phone", id: "telefon"},
+    ]
 
     return (
         <main className="checkout-container">
             <h1 className="secondary-font main-text-color">{title}</h1>
             <div className="container">
-                <CheckoutAddress title={"Adresa dostave"}/>
+                <CheckoutAddress title={"Adresa dostave"} checkoutForm={checkoutForm} />
                 <CheckoutCart />
             </div>
         </main>
--- src/components/Checkout/CheckoutAddress.jsx
+++ src/components/Checkout/CheckoutAddress.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function CheckoutAddress({ title }) {
+function CheckoutAddress({ title, checkoutForm }) {
 
     const [formValues, setFormValues] = useState({
         ime: "",
@@ -38,7 +38,7 @@ function CheckoutAddress({ title }) {
         if (isFormValid) {
             console.log("Naruždba je uspješna!");
         } else {
-            alert("Molim Vas da ispunite sva polja.");
+            alert("Molim Vas da ispunite sva obavezna polja.");
         }
     };
 
@@ -46,40 +46,22 @@ function CheckoutAddress({ title }) {
         <div className="placanje">
             <form className="payform">
                 <legend className="secondary-font main-text-color">{title}</legend>
-                    <label htmlFor="ime">Ime i prezime: <br />
-                        <input type="text" id="ime" name="ime" value={formValues.ime} onChange={handleInputChange} required />
+                {checkoutForm.map((item, index) => {
+                    return(
+                    <>
+                    <label htmlFor={item.id}>{item.title} *<br />
+                        <input type={item.type} id={item.id} name={item.id} value={formValues[item.id]} onChange={handleInputChange} required />
                     </label> <br /><br />
-
-                    <label htmlFor="email">E-mail adresa: <br />
-                        <input type="email" id="email" name="email" value={formValues.email} onChange={handleInputChange} required />
-                    </label> <br /><br />
-
-                    <label htmlFor="adresa">Kućna adresa: <br />
-                        <input type="text" id="adresa" name="adresa" value={formValues.adresa} onChange={handleInputChange} required />
-                    </label> <br /><br />
-
-                    <label htmlFor="grad">Grad: <br />
-                        <input type="text" id="grad" name="grad" value={formValues.grad} onChange={handleInputChange} required />
-                    </label> <br /><br />
-
-                    <label htmlFor="postbroj">Poštanski broj: <br />
-                        <input type="number" id="postbroj" name="postbroj" value={formValues.postbroj} onChange={handleInputChange} required />
-                    </label> <br /><br />
-
-                    <label htmlFor="drzava">Država: <br />
-                        <input type="text" id="drzava" name="drzava" value={formValues.drzava} onChange={handleInputChange} required />
-                    </label> <br /><br />
-
-                    <label htmlFor="telefon">Telefon: <br />
-                        <input type="phone" id="telefon" name="telefon" value={formValues.telefon} onChange={handleInputChange} required />
-                    </label> <br /><br />
+                    </>
+                    )
+                })}
 
                 <label htmlFor="instrukcije">Instrukcije za dostavu:<br />
                     <textarea rows="5" cols="30" maxLength="400"
                         placeholder="Možete unijeti najviše 400 znakova" id="instrukcije"></textarea>
                 </label> <br /><br />
 
-                <legend className="secondary-font main-text-color">Način plaćanja:</legend>
+                <legend className="secondary-font main-text-color">Način plaćanja: *</legend>
                         <input type="radio" id="kartica" name="placanje" value="kartica" onChange={handleInputChange} required />
                         <label htmlFor="kartica">Kreditna kartica</label><br />
 
@@ -104,7 +86,7 @@ function CheckoutAddress({ title }) {
                     />
                     <span className="privola">Prihvaćam prikupljanje i obradu osobnih podataka danih u
                         obrascu bez kojih moj zahtjev ne može biti ispunjen, te da me CyberShop kontaktira u
-                        svrhu slanja obavijesti.</span>
+                        svrhu slanja obavijesti. *</span>
                 </label><br /><br />
 
                 <label htmlFor="newsletter">
